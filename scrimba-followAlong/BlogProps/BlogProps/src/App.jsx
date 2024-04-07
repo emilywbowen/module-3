7a026ee76e0b735418215c43fab5f022bf6dbeb2
Emily Bowen <emilywbowen@gmail.com>
2024-04-07 12:51:58 -0500

    help meh ash

---
+++ scrimba-followAlong/BlogProps/BlogProps/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <h4>This is a test</h4>
+    </div>
+  )
+}
+
+export default App
---
+++ scrimba-followAlong/BlogProps/BlogProps/src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import BlogList from './components/BlogList.jsx'
+import BlogPost from './components/BlogPost.jsx'
+// import Navbar from './components/Navbar.jsx'
+import Header from './components/Header.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    
+    {/* <Navbar /> */}
+    <Header />
+    <BlogPost />
+    <BlogList />
+  </React.StrictMode>,
+)
