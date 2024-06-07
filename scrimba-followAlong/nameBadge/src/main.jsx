b100a07f115f55a02112e39b100c019305c42b57
Emilywbowen <emilywbowen@gmail.com>
2024-06-07 13:44:47 -0500

    cleaned up some styling

--- scrimba-followAlong/nameBadge/src/main.jsx
+++ scrimba-followAlong/nameBadge/src/main.jsx
@@ -1,13 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
+import ReactDOM from 'react-dom'
+import App from './App';
 import './index.css'
-import InputForm from './assets/InputForm.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <InputForm />
-    {/* Need to figure out how to display the input data */}
-  </React.StrictMode>,
-)
+ReactDOM.render(<App />, document.getElementById("root"));
+
